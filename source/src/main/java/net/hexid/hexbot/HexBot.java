818368f983a1032447dc3c95bd96c2f95b0da533
hexid <nick.currier@gmail.com>
2013-02-11 23:06:25 -0700

    gui loads when no arguments are passed

--- source/src/main/java/net/hexid/hexbot/HexBot.java
+++ source/src/main/java/net/hexid/hexbot/HexBot.java
@@ -15,8 +15,10 @@ public class HexBot {
 	public static void main(String[] args) {
 		new HexBot();
 		Bots.removeInvalidBots();
-		if(args.length >= 1 && args[0].equals("gui")) {
+		if((args.length >= 1 && args[0].equals("gui"))) {
 			BotGUI.init(Arrays.copyOfRange(args, 1, args.length));
+		} else if(args.length == 0) {
+			BotGUI.init(args);
 		} else {
 			BotCLI.init(args);
 		}
