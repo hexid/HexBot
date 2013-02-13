5237b23b916422f29f128880494b505283003591
hexid <nick.currier@gmail.com>
2013-02-13 14:03:20 -0700

    Allow 1 argument in Imgur CLI

--- source/src/main/java/net/hexid/hexbot/bots/Imgur.java
+++ source/src/main/java/net/hexid/hexbot/bots/Imgur.java
@@ -1,7 +1,11 @@
 package net.hexid.hexbot.bots;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+import net.hexid.Utils;
+import net.hexid.hexbot.bot.Bots;
 
 public class Imgur implements net.hexid.hexbot.bot.Bot {
 	private ArrayList<String> botArgs;
@@ -11,11 +15,12 @@ public class Imgur implements net.hexid.hexbot.bot.Bot {
 
 	public List<String> getBotExecuteData() {
 		if(botArgs.size() == 1) {
-			//botArgs.add(Bots.)
+			botArgs.add("--output=" + Utils.join(File.separator, Bots.getPWD().getPath(), "output", "Imgur"));
 		} else if(botArgs.size() != 2) {
 			System.out.println("botArgs: album [output]");
 			System.exit(1);
 		}
+		System.out.println(Arrays.toString(botArgs.toArray()));
 		return botArgs;
 	}
 	public void processExitCode(int exitCode) {
