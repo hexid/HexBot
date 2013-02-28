7178989d30d10e489b8d219ac87dadf5678b6709
hexid <nick.currier@gmail.com>
2013-02-28 10:25:22 -0700

    Fix spacing in Utils class

--- source/src/main/java/net/hexid/Utils.java
+++ source/src/main/java/net/hexid/Utils.java
@@ -1,13 +1,11 @@
 package net.hexid;
 
-import java.util.List;
-
 public class Utils {
 	/**
 	 * Combine strings with a common string
 	 * @param glue
 	 * @param str
-	 * @return A single string 
+	 * @return A single string
 	 */
 	public static String join(String glue, String... str) {
 		StringBuilder sb = new StringBuilder();
@@ -16,14 +14,14 @@ public class Utils {
 			sb.append(str[i] + glue);
 		return sb.toString() + str[i];
 	}
-	
+
 	/**
 	 * Combine a list of strings with a common string
 	 * @param glue
 	 * @param str
 	 * @return {@link #join(String, String...)}
 	 */
-	public static String join(String glue, List<String> str) {
+	public static String join(String glue, java.util.List<String> str) {
 		return join(glue, str.toArray(new String[0]));
 	}
 }
