101f72b9d164d649c5bfc2d177ffa94505c4748b
hexid <nick.currier@gmail.com>
2013-04-01 13:58:15 -0600

    Add java files for the Molten bot

---
+++ source/src/main/java/net/hexid/hexbot/bots/Molten.java
@@ -0,0 +1,27 @@
+package net.hexid.hexbot.bots;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Molten implements net.hexid.hexbot.bot.Bot {
+	private ArrayList<String> botArgs;
+	public Molten(ArrayList<String> botArgs) {
+		this.botArgs = botArgs;
+	}
+
+	public List<String> getBotExecuteData() {
+		if(botArgs.size() != 1) {
+			System.out.println("botArgs: username");
+			System.exit(1);
+		} else {
+			botArgs.add("--password="+new String(System.console().readPassword("Password: ")));
+		}
+		return botArgs;
+	}
+	public void processExitCode(int exitCode) {
+		
+	}
+	public String getShortName() {
+		return "Molten";
+	}
+}
