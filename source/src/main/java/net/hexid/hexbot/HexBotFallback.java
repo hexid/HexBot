bcba3e63fa8178adc05b173b15f53afbdeaef862
hexid <nick.currier@gmail.com>
2013-07-09 10:59:14 -0600

    Add a fallback that gets run when JavaFX is not present

---
+++ source/src/main/java/net/hexid/hexbot/HexBotFallback.java
@@ -0,0 +1,26 @@
+package net.hexid.hexbot;
+
+import java.util.Arrays;
+import net.hexid.hexbot.bot.Bots;
+import net.hexid.hexbot.bot.cmd.BotCMD;
+
+public class HexBotFallback extends HexBot {
+	public HexBotFallback() {
+		super();
+	}
+
+	protected void init(String[] args) {
+		Bots.removeInvalidBots();
+
+		if(args.length > 0 && args[0].equalsIgnoreCase("gui")) {
+			args = Arrays.copyOfRange(args, 1, args.length);
+		}
+
+		BotCMD.init(args);
+	}
+
+	public static void main(String[] args) {
+		HexBotFallback hexbotFallback = new HexBotFallback();
+		hexbotFallback.init(args);
+	}
+}
