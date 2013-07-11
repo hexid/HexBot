305d4348fab8144df5a0ad9f951dca9796de3708
hexid <nick.currier@gmail.com>
2013-07-11 13:08:28 -0600

    Rename getShortName() to getBotID() in Bot interface

--- source/src/main/java/net/hexid/hexbot/bot/Bot.java
+++ source/src/main/java/net/hexid/hexbot/bot/Bot.java
@@ -5,14 +5,14 @@ public interface Bot {
 	 * @return Console parameters for the bot
 	 */
 	public java.util.List<String> getBotExecuteData();
-	
+
 	/**
 	 * @param exitCode Code returned when the Process exits
 	 */
 	public void processExitCode(int exitCode);
-	
+
 	/**
-	 * @return The name of the bot that is its key in {@link net.hexid.hexbot.bot.Bots}
+	 * @return The id of the bot that is its key in {@link net.hexid.hexbot.bot.Bots}
 	 */
-	public String getShortName();
+	public String getBotID();
 }
