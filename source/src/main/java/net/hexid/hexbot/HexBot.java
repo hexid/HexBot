f56d9b5d8e92964c0d0b879f128c4ed6db5d991b
hexid <nick.currier@gmail.com>
2013-07-11 18:51:43 -0600

    Update main launcher to use new classes for command line and gui interfaces

--- source/src/main/java/net/hexid/hexbot/HexBot.java
+++ source/src/main/java/net/hexid/hexbot/HexBot.java
@@ -7,13 +7,13 @@ import net.hexid.hexbot.bot.cmd.BotCMD;
 
 public class HexBot {
 	public HexBot() {
-		Bots.addBot("astral", "Astral WoW", "net.hexid.hexbot.bots.Astral", "net.hexid.hexbot.bots.AstralTab", "Astral.coffee");
-		Bots.addBot("bing", "Bing Rewards", "net.hexid.hexbot.bots.Bing", "net.hexid.hexbot.bots.BingTab", "Bing.coffee");
-		Bots.addBot("imgur", "Imgur Albums", "net.hexid.hexbot.bots.Imgur", "net.hexid.hexbot.bots.ImgurTab", "Imgur.coffee");
-		Bots.addBot("molten", "Molten WoW", "net.hexid.hexbot.bots.Molten", "net.hexid.hexbot.bots.MoltenTab", "Molten.coffee");
-		Bots.addBot("test-coffee", "Test-CF", "net.hexid.hexbot.bots.Test", "net.hexid.hexbot.bots.TestTab", "Test.coffee");
-		Bots.addBot("test-js", "Test-JS", "net.hexid.hexbot.bots.Test", "net.hexid.hexbot.bots.TestTab", "Test.js");
-		Bots.addBot("xbox", "Xbox Codes", "net.hexid.hexbot.bots.Xbox", "net.hexid.hexbot.bots.XboxTab", "Xbox.coffee");
+		Bots.addBot("astral", "Astral WoW", "net.hexid.hexbot.bots.cmd.generic.UserPass", "net.hexid.hexbot.bots.gui.AstralTab", "Astral.coffee");
+		Bots.addBot("bing", "Bing Rewards", "net.hexid.hexbot.bots.cmd.Bing", "net.hexid.hexbot.bots.gui.BingTab", "Bing.coffee");
+		Bots.addBot("imgur", "Imgur Albums", "net.hexid.hexbot.bots.cmd.Imgur", "net.hexid.hexbot.bots.gui.ImgurTab", "Imgur.coffee");
+		Bots.addBot("molten", "Molten WoW", "net.hexid.hexbot.bots.cmd.generic.UserPass", "net.hexid.hexbot.bots.gui.MoltenTab", "Molten.coffee");
+		Bots.addBot("test-coffee", "Test-CF", "net.hexid.hexbot.bots.cmd.generic.NoArgs", "net.hexid.hexbot.bots.gui.TestTab", "Test.coffee");
+		Bots.addBot("test-js", "Test-JS", "net.hexid.hexbot.bots.cmd.generic.NoArgs", "net.hexid.hexbot.bots.gui.TestTab", "Test.js");
+		Bots.addBot("xbox", "Xbox Codes", "net.hexid.hexbot.bots.cmd.Xbox", "net.hexid.hexbot.bots.gui.XboxTab", "Xbox.coffee");
 	}
 
 	protected void init(String[] args) {
