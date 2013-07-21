75a02d7ef75540407612a614b5d4affe45a5e84c
hexid <nick.currier@gmail.com>
2013-07-21 13:44:43 -0600

    Allow use of new BotTab in the BotTabPane

--- source/src/main/java/net/hexid/hexbot/bot/gui/BotTabPane.java
+++ source/src/main/java/net/hexid/hexbot/bot/gui/BotTabPane.java
@@ -1,6 +1,7 @@
 package net.hexid.hexbot.bot.gui;
 
 import javafx.event.Event;
+import javafx.scene.control.Tab;
 
 public class BotTabPane extends javafx.scene.control.TabPane {
 	public BotTabPane() {
@@ -8,11 +9,12 @@ public class BotTabPane extends javafx.scene.control.TabPane {
 		setSide(javafx.geometry.Side.BOTTOM); // have the tabs along the bottom
 	}
 
-	public void addBotTab(final BotTab tab) {
+	public void addBotTab(final BotTab botTab) {
+		Tab tab = botTab.getTab();
 		// kill the process and remove the tab from the list when closed
 		tab.setOnClosed(new javafx.event.EventHandler<Event>() {
 			public void handle(Event e) {
-				tab.killProcess(); // stop the bot process in the tab
+				botTab.killProcess(); // stop the bot process in the tab
 			}
 		});
 		getTabs().add(tab);
