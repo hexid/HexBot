5015ebe253d1a34cbcb4491295be147b059ab38e
hexid <nick.currier@gmail.com>
2013-07-21 13:11:30 -0600

    Change Bot from interface to abstract class and give it a botID String and getter method

--- source/src/main/java/net/hexid/hexbot/bot/Bot.java
+++ source/src/main/java/net/hexid/hexbot/bot/Bot.java
@@ -1,18 +1,26 @@
 package net.hexid.hexbot.bot;
 
-public interface Bot {
-	/**
-	 * @return Console parameters for the bot
-	 */
-	public java.util.List<String> getBotExecuteData();
+public abstract class Bot {
+	String botID;
 
-	/**
-	 * @param exitCode Code returned when the Process exits
-	 */
-	public void processExitCode(int exitCode);
+	public Bot(String botID) {
+		this.botID = botID;
+	}
 
 	/**
 	 * @return The id of the bot that is its key in {@link net.hexid.hexbot.bot.Bots}
 	 */
-	public String getBotID();
+	public String getBotID() {
+		return botID;
+	}
+
+	/**
+	 * @return Console parameters for the bot
+	 */
+	public abstract java.util.List<String> getBotExecuteData();
+
+	/**
+	 * @param exitCode Code returned when the Process exits
+	 */
+	public abstract void processExitCode(int exitCode);
 }
